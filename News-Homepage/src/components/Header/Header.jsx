c9cc0931d4de6a93e7117b950884b65025340c43
fabio vitharana <fabio17.vitharana@gmail.com>
2022-12-17 23:45:16 +0100

    header added

--- News-Homepage/src/components/Header/Header.jsx
+++ News-Homepage/src/components/Header/Header.jsx
@@ -1,10 +1,20 @@
 import './_header.scss';
+import logo from '../../assets/images/logo.svg';
 
-function Header(){
+function Header() {
 
-    return(
+    return (
         <>
-        <h1>Header</h1>
+            <header className='header'>
+                <img src={logo} alt="logo" className='header__logo' />
+                <ul className="header__nav">
+                    <li className="header__item"> <a href="#" className="header__link">Home</a></li>
+                    <li className="header__item"> <a href="#" className="header__link">New</a></li>
+                    <li className="header__item"> <a href="#" className="header__link">Popular</a></li>
+                    <li className="header__item"> <a href="#" className="header__link">Trending</a></li>
+                    <li className="header__item"> <a href="#" className="header__link">Categories</a></li>
+                </ul>
+            </header>
         </>
     )
 }

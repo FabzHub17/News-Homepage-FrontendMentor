58c3172707939033d9bb5668ae0169e277f68792
fabio vitharana <fabio17.vitharana@gmail.com>
2022-12-18 15:16:04 +0100

    Hero section main contents added (unstyles)

---
+++ News-Homepage/src/components/Hero/Hero.jsx
@@ -0,0 +1,14 @@
+import HeroContent from '../HeroContent/HeroContent';
+import './_hero.scss';
+
+function Hero (){
+    return(
+        <>
+        <section className="hero">
+            <HeroContent />
+        </section>
+        </>
+    )
+}
+
+export default Hero;
---
+++ News-Homepage/src/components/HeroContent/HeroContent.jsx
@@ -0,0 +1,18 @@
+import './_heroContent.scss';
+import heroImg from '../../assets/images/image-web-3-desktop.jpg';
+
+function HeroContent() {
+    return (
+        <>
+            <section className="hero__main">
+                <img src={heroImg} alt="" className="hero__img" />
+                <h1 className="hero__heading heading heading--primary">The Bright Future of Web 3.0?</h1>
+                <p className="hero__paragraph paragraph"> We dive into the next evolution of the web that claims to put the power of the platforms back into the hands of the people.
+                    But is it really fulfilling its promise?</p>
+                <a href="#" className="hero__btn btn btn--primary">Read more</a>
+            </section>
+        </>
+    )
+}
+
+export default HeroContent;

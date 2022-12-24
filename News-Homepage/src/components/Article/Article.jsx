4667ca43787bc13053bd4613b08239c9476b0889
fabio vitharana <fabio17.vitharana@gmail.com>
2022-12-24 22:19:05 +0100

    news dashboard completed

--- News-Homepage/src/components/Article/Article.jsx
+++ News-Homepage/src/components/Article/Article.jsx
@@ -1,9 +1,9 @@
-function Article(){
+function Article({title,body}){
     return(
         <>
         <article className="article">
-            <h3 className="article__heading heading heading--tertiary" name='heading'>heading</h3>
-            <p className="article__paragraph paragraph" name='paragraph'>paragraph</p>
+            <h3 className="article__heading heading heading--tertiary" name='title'>{title}</h3>
+            <p className="article__paragraph paragraph" name='body'>{body}</p>
         </article>
         </>
     )
--- News-Homepage/src/components/NewsDashboard/NewsDashboard.jsx
+++ News-Homepage/src/components/NewsDashboard/NewsDashboard.jsx
@@ -1,15 +1,15 @@
 import Article from '../Article/Article';
 import './_newsDashboard.scss';
 
-function NewsDashboard(){
-    return(
+function NewsDashboard() {
+    return (
         <>
-         <div className="news__container">
-            <h1 className="news__heading heading heading--secondary">New</h1>
-            <Article />
-            <Article />
-            <Article />
-         </div>
+            <div className="news__container">
+                <h1 className="news__heading heading heading--secondary">New</h1>
+                <Article title="Hydrogen VS Electric Cars" body="Will hydrogen-fuelled cars ever catch up to EVs?" />
+                <Article title="The Downsides of AI Artistry" body="What are the possible adverse effects of on-demand AI image generation?"/>
+                <Article title="Is VC Funding Drying Up?" body="Private funding by VC firm is down 50% YOY.We take a look at what that means"/>
+            </div>
         </>
     )
 }

a57ec27663cdd7fea668944476e3316de73059e5
fabio vitharana <fabio17.vitharana@gmail.com>
2023-01-03 15:37:37 +0100

    hero contents basic desktop grid layout set up

--- News-Homepage/src/components/HeroContent/HeroContent.jsx
+++ News-Homepage/src/components/HeroContent/HeroContent.jsx
@@ -5,7 +5,9 @@ function HeroContent() {
     return (
         <>
             <section className="hero__main">
-                <img src={heroImg} alt="" className="hero__img" />
+                <div className="hero__img__container">
+                    <img src={heroImg} alt="" className="hero__img" />
+                </div>
                 <h1 className="hero__heading heading heading--primary">The Bright Future of Web 3.0?</h1>
                 <p className="hero__paragraph paragraph"> We dive into the next evolution of the web that claims to put the power of the platforms back into the hands of the people.
                     But is it really fulfilling its promise?</p>
